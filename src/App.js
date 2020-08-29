e215636e7041304e30608fa61feef0aeff09a6f9
JadeTyrer <jadetyrer@hotmail.co.uk>
2020-08-29 11:50:28 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Counter from './components/Counter'
+import { GlobalStyle } from './components/StyledComponents'
 
 const App = () => {
   return (
     <div >
-          The basic empty template
+      <GlobalStyle />
+      <Counter />
     </div>
   )
 }
